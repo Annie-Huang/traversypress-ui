4eac93dc8e4bcf278f6f7c1a6fb9faffd4eed41f
Annie Huang <annie.hn@gmail.com>
2024-06-20 17:21:46 +1000

    Add logo into the Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../img/logo.png';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='bg-primary dark:bg-slate-700 text-white py-2 px-5 flex justify-between'>
+      <Link href='/'>
+        <Image src={logo} alt='TraversyPress' width={40} />
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
