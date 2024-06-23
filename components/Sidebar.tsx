6ad7ce91a06eb4477145fffdee75dd7865a03f43
Annie Huang <annie.hn@gmail.com>
2024-06-23 12:41:05 +1000

    Copy the <Command> code from https://ui.shadcn.com/docs/components/command into <Sidebar> and make it wider in layout.

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -20,7 +20,25 @@ import {
 import Link from 'next/link';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <Command>
+      <CommandInput placeholder='Type a command or search...' />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        <CommandGroup heading='Suggestions'>
+          <CommandItem>Calendar</CommandItem>
+          <CommandItem>Search Emoji</CommandItem>
+          <CommandItem>Calculator</CommandItem>
+        </CommandGroup>
+        <CommandSeparator />
+        <CommandGroup heading='Settings'>
+          <CommandItem>Profile</CommandItem>
+          <CommandItem>Billing</CommandItem>
+          <CommandItem>Settings</CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+  );
 };
 
 export default Sidebar;
