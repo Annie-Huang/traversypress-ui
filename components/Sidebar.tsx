ec680f7ef0c29fcc8f0662178b599055eb5cfc9b
Annie Huang <annie.hn@gmail.com>
2024-06-23 12:38:25 +1000

    Copy import required by <Sidebar>

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,24 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from '@/components/ui/command';
+import {
+  LayoutDashboard,
+  Newspaper,
+  Folders,
+  CreditCard,
+  Settings,
+  User,
+} from 'lucide-react';
+import Link from 'next/link';
+
 const Sidebar = () => {
   return <div>Sidebar</div>;
 };
