823b11969659496fb433fe9c99bd09b4b78fa7bc
Annie Huang <annie.hn@gmail.com>
2024-06-24 17:49:10 +1000

    Create the icon and count text in <DashboardCard>

--- components/dashboard/DashboardCard.tsx
+++ components/dashboard/DashboardCard.tsx
@@ -8,6 +8,12 @@ const DashboardCard = () => {
         <h3 className='text-3xl text-center mb-4 font-bold text-slate-500 dark:text-slate-200'>
           Posts
         </h3>
+        <div className='flex gap-5 justify-center items-center'>
+          <Newspaper className='text-slate-500' size={72} />
+          <h3 className='text-5xl font-semibold text-slate-500 dark:text-slate-200'>
+            100
+          </h3>
+        </div>
       </CardContent>
     </Card>
   );
