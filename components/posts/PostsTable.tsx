990c648a79700df5640ca64424daa7ff39949acc
Annie Huang <annie.hn@gmail.com>
2024-06-27 17:24:18 +1000

    Add all the import for <PostsTable>

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -1,4 +1,15 @@
-import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  TableCaption,
+} from '@/components/ui/table';
+import Link from 'next/link';
+import posts from '@/data/posts';
+import { Post } from '@/types/posts';
 
 const PostsTable = () => {
   return <div>Posts</div>;
