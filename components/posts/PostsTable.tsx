d94d4d02f6965274641fbe6e1de0951b6c847f2a
Annie Huang <annie.hn@gmail.com>
2024-06-27 18:02:00 +1000

    Add filter and limit on the posts data

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -18,6 +18,14 @@ interface PostsTableProps {
 
 // const PostsTable = () => {
 const PostsTable = ({ limit, title }: PostsTableProps) => {
+  // Sort posts in dec order based on date
+  const sortedPosts: Post[] = [...posts].sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+
+  // Filter posts to limit
+  const filteredPosts = limit ? sortedPosts.slice(0, limit) : sortedPosts;
+
   return (
     <div className='mt-10'>
       <h3 className='text-2xl mb-4 font-semibold'>{title ? title : 'Posts'}</h3>
@@ -34,7 +42,7 @@ const PostsTable = ({ limit, title }: PostsTableProps) => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {posts.map((post) => (
+          {filteredPosts.map((post) => (
             <TableRow key={post.id}>
               <TableCell>{post.title}</TableCell>
               <TableCell className='hidden md:table-cell'>
