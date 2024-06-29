fe422526b83bbaf8677e4edb0d4cdf5b3697cc05
Annie Huang <annie.hn@gmail.com>
2024-06-29 14:53:19 +1000

    Make <AnalyticsChart> a client component in order to use the chart. and add all the import

--- components/dashboard/AnalyticsChart.tsx
+++ components/dashboard/AnalyticsChart.tsx
@@ -1,4 +1,23 @@
-import React from 'react';
+'use client';
+// Need to be a client component in order to use the chart.
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import data from '@/data/analytics';
 
 const AnalyticsChart = () => {
   return <div>Chart</div>;
