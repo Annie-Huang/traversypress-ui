497883a708ce1cc71c8f3336ead992a63d47ffff
Annie Huang <annie.hn@gmail.com>
2024-06-29 14:56:29 +1000

    Add the CardHeader into the template

--- components/dashboard/AnalyticsChart.tsx
+++ components/dashboard/AnalyticsChart.tsx
@@ -20,7 +20,14 @@ import {
 import data from '@/data/analytics';
 
 const AnalyticsChart = () => {
-  return <div>Chart</div>;
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Analytics For This Year</CardTitle>
+        <CardDescription>Views Per Month</CardDescription>
+      </CardHeader>
+    </Card>
+  );
 };
 
 export default AnalyticsChart;
