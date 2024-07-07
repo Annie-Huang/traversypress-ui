edaa95a991feb8877e85557fd3cf04e45a5a47fa
Annie Huang <annie.hn@gmail.com>
2024-07-07 15:47:10 +1000

    Create (main)/posts/edit/[id]/page.tsx. Add BackButton and add all the input for Form and zod.

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import BackButton from '@/components/BackButton';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import posts from '@/data/posts';
+
+const Page = () => {
+  return (
+    <>
+      <BackButton text='Back to Posts' link='/posts' />
+    </>
+  );
+};
+
+export default Page;
