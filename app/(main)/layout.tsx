09b358543a99eaafde967aa86952daf0ad383b01
Annie Huang <annie.hn@gmail.com>
2024-08-16 17:14:06 +1000

    Shift the <Navbar>, <Sidebar>, main area from app/layout.tsx to app/(main)/layout.tsx

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='h-[100vh] flex items-center justify-center relative'>
-      {children}
-    </div>
+    <>
+      <Navbar />
+      <div className='flex'>
+        <div className='hidden md:block h-[100vh] w-[300px]'>
+          <Sidebar />
+        </div>
+        <div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
+      </div>
+    </>
   );
 };
 
